79db5be98181efa98127c1f3938a9d45de7ad459
wtl666wtl <2351090351@qq.com>
2019-12-25 15:52:11 +0800

    Update bookstore.cpp

--- bookstore.cpp
+++ bookstore.cpp
@@ -823,10 +823,10 @@ int main() {
 	e[1].mins="\0";
 	e[1].maxs="\0";
 	Book ttmp=Book("\0","@\0","@\0","@\0",0,0);
-	file_name.write(reinterpret_cast<const char *>(&ttmp),sizeof(Book));
+	//file_name.write(reinterpret_cast<const char *>(&ttmp),sizeof(Book));
 	file_name.close();
 	Name tmp=ok("root","sjtu","7","root");
-	findu(tmp);
+	//findu(tmp);
 	//work("orz");
 	//if(!command) {
 		//orz;
