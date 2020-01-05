272946c7a77639476c118d6142597320b5ee1763
wtl666wtl <2351090351@qq.com>
2020-01-05 18:48:14 +0800

    Update bookstore.cpp

--- bookstore.cpp
+++ bookstore.cpp
@@ -822,5 +822,5 @@ int main() {
 	ofstream file_name("1.in",ofstream::out|ofstream::binary);
 	char orz[101];
 	orz[0]='a';orz[1]='c';orz[2]='\0';
-	file_name.write((char*)(orz),3);
+	file_name.write((char*)(orz),sizeof(orz));
 }
